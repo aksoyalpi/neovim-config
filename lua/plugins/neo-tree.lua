c99aa0f3404b665e2a194b09d8872c1def4927d9
Alperen Aksoy <aksoyalpi@gmail.com>
2025-07-01 19:53:46 +0200

    first commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return   {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"1rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    -- fill any relevant options here
+  },
+  config = function()
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+  end
+}
